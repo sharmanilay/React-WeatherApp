740b25fb9355afd548a5c8d39505702375bd40b1
Nilay Sharma <nilaysharma642@gmail.com>
2018-08-17 11:26:46 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '57fe8374eb83450809935a740412025f';
+const API_KEY = 'Your API KEY';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
